ee738b08f7c626a50b67969764c1650d09fc2c95
VieTGioIHOa <vietntph22574@fpt.edu.vn>
2022-06-28 15:58:58 +0700

    fixed header

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.all('/', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
-});
+})
 
 app.use(express.json())
 
