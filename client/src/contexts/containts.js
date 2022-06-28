8f5061702d97584d3190493d6e155867bf79dfba
VieTGioIHOa <vietntph22574@fpt.edu.vn>
2022-06-28 15:38:15 +0700

    fixed url

--- client/src/contexts/containts.js
+++ client/src/contexts/containts.js
@@ -1,7 +1,7 @@
 export const apiUrl =
     process.env.NODE_ENV !== 'production'
         ? 'http://localhost:5000/api'
-        : 'anyUrl'
+        : 'https://pacific-plains-81115.herokuapp.com'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'mern-stack'
 export const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'
