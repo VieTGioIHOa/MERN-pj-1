f1ae82b996e1b3ee1908c0a066cc466f7ff0d4f1
VieTGioIHOa <vietntph22574@fpt.edu.vn>
2022-06-28 15:49:16 +0700

    fixed has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource

--- server/index.js
+++ server/index.js
@@ -22,6 +22,12 @@ connectDB()
 
 const app = express()
 
+app.all('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+});
+
 app.use(express.json())
 
 app.use(express.urlencoded({
